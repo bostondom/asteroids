c0499398f1f91a2508c13330f540ed00177b8ee0
Domenic Campochiaro <dcampochiaro@fshealth.com>
2025-01-09 20:14:17 -0500

    Asteroids, shots, and death

--- main.py
+++ main.py
@@ -4,18 +4,34 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,11 +39,19 @@ def main():
          
 
         screen.fill("black")
-        print(f"Triangle: {player.triangle()}")
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         
         dt = (clock.tick(60)) / 1000.0
-        player.update(dt)   
+        
+        for object in updatable:
+            object.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                return
+            
         pygame.display.flip()
 
 
